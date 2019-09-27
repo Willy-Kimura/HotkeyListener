13d8c07fe4c4cfea10c6fad1884fffdd5311d4ce
willy_kimura <wilfred.kimura@bunifu.co.ke>
2019-09-27 13:55:20 +0300

    Add project files.

---
+++ HotkeyListener/HotkeyListener.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Windows.Forms;
+using System.ComponentModel;
+using System.Collections.Generic;
+
+namespace WK.Libraries.HotkeyListenerNS
+{
+    public partial class HotkeyListener : Component
+    {
+        #region Constructors
+
+        public HotkeyListener()
+        {
+            InitializeComponent();
+
+            SetDefaults();
+        }
+
+        public HotkeyListener(IContainer container)
+        {
+            container.Add(this);
+
+            InitializeComponent();
+
+            SetDefaults();
+        }
+
+        #endregion
+
+        #region Fields
+
+        #endregion
+
+        #region Properties
+
+        #region Browsable
+
+        #endregion
+
+        #region Non-browsable
+
+        #endregion
+
+        #endregion
+
+        #region Methods
+
+        #region Public
+
+        #endregion
+
+        #region Private
+
+        private void SetDefaults()
+        {
+
+        }
+
+        #endregion
+
+        #endregion
+
+        #region Events
+
+        #region Public
+
+        #endregion
+
+        #endregion
+    }
+}
